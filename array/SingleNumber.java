92b495a7057c769a8f0b0b7b00be8962daa9dd89
PIAL MOLLAH <pial.mollah.0401@gmail.com>
2025-12-16 15:23:28 +0600

    Leetcode problem 136 (single number) v1

--- array/SingleNumber.java
+++ array/SingleNumber.java
@@ -1,20 +1,9 @@
 package array;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
 public class SingleNumber {
     public static void main(String[] args) {
-
-        /*
-         * Given a non-empty array of integers nums, every element appears twice except
-         * for one. Find that single one.
-         * You must implement a solution with a linear runtime complexity and use only
-         * constant extra space.
-         * Input: nums = [4,1,2,1,2]
-         * Output: 4
-         */
         int[] nums = { 4, 1, 2, 1, 2 };
         System.out.println(singleNumber(nums));
     }
