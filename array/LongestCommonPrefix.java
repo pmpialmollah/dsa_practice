6f920fe4793b63a6ade4ca55f13110d1f4bfbeab
PIAL MOLLAH <pial.mollah.0401@gmail.com>
2025-12-16 11:26:33 +0600

    Leetcode problem 14 (longest common prefix) v1

---
+++ array/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+package array;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] strs = { "flower", "flow", "flight" };
+
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (String s : strs) {
+            while (s.indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+}
