b1c3495f6cbbd0cd23a8bf49f531f4f8b8b8d02b
PIAL MOLLAH <pial.mollah.0401@gmail.com>
2026-01-03 19:34:56 +0600

    Cleaning code, modify variable name

--- array/RemoveDuplicates.java
+++ array/RemoveDuplicates.java
@@ -4,23 +4,28 @@ import java.util.Arrays;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
-        System.out.println(removeDuplicates(nums));
-        System.out.println(Arrays.toString(nums));
+
+        int[] numbers = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        System.out.println(removeDuplicates(numbers));
+        System.out.println(Arrays.toString(numbers));
+
     }
 
     public static int removeDuplicates(int[] nums) {
+
         if (nums.length == 1) {
             return 1;
         }
-        int res = 1;
+
+        int totalDistinctNumber = 1;
+
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] != nums[i + 1]) {
-                nums[res] = nums[i + 1];
-                res++;
+                nums[totalDistinctNumber] = nums[i + 1];
+                totalDistinctNumber++;
             }
         }
 
-        return res;
+        return totalDistinctNumber;
     }
 }
